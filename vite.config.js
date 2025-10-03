2e8efe5eb0b1aa20498c84103ac4ddb839685ec5
Farhan ullah <farhansalikh@gmail.com>
2025-10-03 04:26:10 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': '/src'
+    }
   },
+  build: {
+    outDir: 'dist'
+  }
 });
